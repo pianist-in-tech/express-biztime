ad0da8816e52c5a9a547705966de65a5be1456cd
Larisa Capatina <117665726+pianist-in-tech@users.noreply.github.com>
2023-07-25 11:23:27 -0600

    added files

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const ExpressError = require("./expressError")
+const companiesRoutes = require("./routes/companies");
+const invoicesRoutes = require("./routes/invoices");
+
+const app = express();
+
+app.use(express.json());
+app.use("/companies", companiesRoutes);
+app.use("/invoices", invoicesRoutes);
+
+app.use(function (req, res, next) {
+  const err = new ExpressError("Not Found", 404);
+  return next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err,
+    message: err.message
+  });
+});
+
+
+module.exports = app;
